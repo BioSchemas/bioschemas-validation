19c6e3512727b9f19c480adb0becbf6e25cdb50f
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2022-01-06 12:00:54 +0100

    help message

--- bioschemas_shacl/main.py
+++ bioschemas_shacl/main.py
@@ -8,8 +8,6 @@ from rich.table import Table
 from tabulate import tabulate
 from rich.text import Text
 
-# from rich.progress import track
-
 from bioschemas_profile_shape.WebResource import WebResource
 from bioschemas_profile_shape.bioschemas_shape_gen import validate_any_from_microdata
 
@@ -17,8 +15,8 @@ parser = argparse.ArgumentParser(
     description="""
 Bioschemas-shacl-validator, a command line tool to validate bisochemas annotated web resources.
 
-Usage examples :
-    python main.py --u https://bgee.org/?page=gene&gene_id=ENSG00000274928 
+Usage :
+python main.py -u https://bgee.org/?page=gene&gene_id=ENSG00000274928 
 
 Please report any issue to thomas.rosnet@france-bioinforatique.fr, alban.gaignard@univ-nantes.fr
 """,
@@ -42,15 +40,6 @@ parser.add_argument(
     help="list of URLs to be tested",
     dest="urls",
 )
-# parser.add_argument(
-#     "-bs",
-#     "--bioschemas",
-#     action="store_true",
-#     required=False,
-#     help="validate Bioschemas profiles",
-#     dest="bioschemas",
-# )
-
 
 if __name__ == "__main__":
     try:
@@ -89,51 +78,6 @@ if __name__ == "__main__":
                 console.rule(f"[bold red]Bioschemas validation for URL {url}")
                 res = validate_any_from_microdata(input_url=url)
                 console.print(res)
-
-                # print(tabulate({"Warnings": res[id]["warnings"],
-                #                 "Errors": res[id]["warnings"]}, headers = "keys"))
-
-
-                # print(res[url]["errors"])
-                # print(res[url]["warnings"])
-
-                # for m in track(metrics_collection, "Processing FAIR metrics ..."):
-                #     logging.info(m.get_name())
-                #     res = m.evaluate()
-                #     if m.get_name().startswith("F"):
-                #         table.add_row(
-                #             Text(
-                #                 m.get_name() + " " + str(res), style=get_result_style(res)
-                #             ),
-                #             "",
-                #             "",
-                #             "",
-                #         )
-                #     elif m.get_name().startswith("A"):
-                #         table.add_row(
-                #             "",
-                #             Text(
-                #                 m.get_name() + " " + str(res), style=get_result_style(res)
-                #             ),
-                #             "",
-                #             "",
-                #         )
-                #     elif m.get_name().startswith("I"):
-                #         table.add_row(
-                #             "",
-                #             "",
-                #             Text(
-                #                 m.get_name() + " " + str(res), style=get_result_style(res)
-                #             ),
-                #             "",
-                #         )
-                #     elif m.get_name().startswith("R"):
-                #         table.add_row(
-                #             "",
-                #             "",
-                #             "",
-                #             Text(f"{m.get_name()} {str(res)}", style=get_result_style(res)),
-                #         )
 
             console.rule(f"[bold red]Bioschemas validation for URL {url}")
             #console.print(table)
